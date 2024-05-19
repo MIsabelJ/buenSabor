b90babfe8722612df021df93ae2e0d1ef62f15d6
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-05-19 11:08:22 -0300

    @Service

--- src/main/java/com/entidades/buenSabor/business/service/ArticuloInsumoService.java
+++ src/main/java/com/entidades/buenSabor/business/service/ArticuloInsumoService.java
@@ -2,6 +2,8 @@ package com.entidades.buenSabor.business.service;
 
 import com.entidades.buenSabor.business.service.Base.BaseService;
 import com.entidades.buenSabor.domain.entities.ArticuloInsumo;
+import org.springframework.stereotype.Service;
+
 
 public interface ArticuloInsumoService extends BaseService<ArticuloInsumo, Long> {
 }
--- src/main/java/com/entidades/buenSabor/business/service/Imp/ArticuloInsumoServiceImp.java
+++ src/main/java/com/entidades/buenSabor/business/service/Imp/ArticuloInsumoServiceImp.java
@@ -3,8 +3,9 @@ package com.entidades.buenSabor.business.service.Imp;
 import com.entidades.buenSabor.business.service.ArticuloInsumoService;
 import com.entidades.buenSabor.business.service.Base.BaseServiceImp;
 import com.entidades.buenSabor.domain.entities.ArticuloInsumo;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class ArticuloInsumoServiceImp extends BaseServiceImp<ArticuloInsumo, Long> implements ArticuloInsumoService {
 
 }
