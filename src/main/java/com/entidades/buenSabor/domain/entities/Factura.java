adc72abfa4954868e82cd1032b8a5a27ffb8b101
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2024-06-03 20:42:51 -0300

    Facturas Dto

--- src/main/java/com/entidades/buenSabor/domain/entities/Factura.java
+++ src/main/java/com/entidades/buenSabor/domain/entities/Factura.java
@@ -22,13 +22,11 @@ import java.util.List;
 @SuperBuilder
 @Audited
 public class Factura extends Base{
-    private LocalDate fechaFcturacion;
+    private LocalDate fechaFacturacion;
     private Integer mpPaymentId;
     private Integer mpMerchantOrderId;
     private String mpPreferenceId;
     private String mpPaymentType;
     private FormaPago formaPago;
     private Double totalVenta;
-
-
 }
