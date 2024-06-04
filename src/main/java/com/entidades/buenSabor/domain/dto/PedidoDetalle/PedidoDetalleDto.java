4301f89c9579baa40870f6d9c1fa3651dd601896
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-06-04 16:04:53 -0300

    DetallePedido y PedidoDetalle arreglado

--- src/main/java/com/entidades/buenSabor/domain/dto/PedidoDetalle/PedidoDetalleDto.java
+++
@@ -1,5 +0,0 @@
-package com.entidades.buenSabor.domain.dto.PedidoDetalle;
-
-public class PedidoDetalleDto {
-
-}
--- src/main/java/com/entidades/buenSabor/domain/dto/PedidoDetalle/PedidoDetallePostDto.java
+++
@@ -1,7 +0,0 @@
-package com.entidades.buenSabor.domain.dto.PedidoDetalle;
-
-import com.entidades.buenSabor.domain.dto.BaseDto;
-
-public class PedidoDetallePostDto extends BaseDto {
-
-}
