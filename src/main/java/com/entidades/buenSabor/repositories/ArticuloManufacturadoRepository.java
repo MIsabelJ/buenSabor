ce7ff5ec044b4b761731ebe25a296008b9d4d9ef
MIsabelJ <maria.jurman@alumnos.frm.utn.edu.ar>
2024-06-22 00:33:42 -0300

    Arreglo querys para PostgreSQL

--- src/main/java/com/entidades/buenSabor/repositories/ArticuloManufacturadoRepository.java
+++ src/main/java/com/entidades/buenSabor/repositories/ArticuloManufacturadoRepository.java
@@ -12,11 +12,12 @@ import java.util.List;
 
 @Repository
 public interface ArticuloManufacturadoRepository extends BaseRepository<ArticuloManufacturado, Long> {
-    @Query(nativeQuery = true, value = "SELECT a.denominacion AS Comida, SUM(dp.cantidad) AS 'Cantidad Vendida', c" +
+    @Query(nativeQuery = true, value = "SELECT a.denominacion AS Comida, SUM(dp.cantidad) AS \"Cantidad Vendida\", c" +
             ".denominacion AS Categoria, s.nombre AS Sucursal FROM pedido p JOIN detalle_pedido dp ON p.id = dp" +
             ".pedido_id JOIN sucursal s ON p.sucursal_id = s.id JOIN articulo a ON dp.articulo_id = a.id JOIN " +
             "articulo_manufacturado am ON a.id = am.id JOIN categoria c ON am.categoria_id = c.id WHERE p" +
-            ".fecha_pedido BETWEEN :fechaDesde AND :fechaHasta GROUP BY am.id ORDER BY SUM(dp.cantidad) DESC LIMIT 15;")
+            ".fecha_pedido BETWEEN :fechaDesde AND :fechaHasta GROUP BY am.id, a.denominacion, c.denominacion, s" +
+            ".nombre ORDER BY SUM(dp.cantidad) DESC LIMIT 15")
     List<Object[]> obtenerComidasMasPedidas(@Param("fechaDesde") LocalDate fechaDesde, @Param(
             "fechaHasta") LocalDate fechaHasta);
 
--- src/main/java/com/entidades/buenSabor/repositories/PedidoRepository.java
+++ src/main/java/com/entidades/buenSabor/repositories/PedidoRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 
 @Repository
 public interface PedidoRepository extends BaseRepository<Pedido, Long> {
-    @Query(nativeQuery = true, value = "SELECT fecha_pedido AS Fecha, id as 'Numero Pedido', total AS 'Total Venta', " +
-            "CASE forma_pago WHEN 0 THEN 'EFECTIVO' WHEN 1 THEN 'MERCADO PAGO' END AS 'Forma de Pago' FROM Pedido p " +
+    @Query(nativeQuery = true, value = "SELECT fecha_pedido AS Fecha, id as \"Numero Pedido\", total AS \"Total Venta\", " +
+            "CASE forma_pago WHEN 0 THEN 'EFECTIVO' WHEN 1 THEN 'MERCADO PAGO' END AS \"Forma de Pago\" FROM Pedido p " +
             "WHERE fecha_pedido = :dia ORDER BY id")
     List<Object[]> ingresosDiarios(@Param("dia") LocalDate dia);
 
@@ -20,14 +20,14 @@ public interface PedidoRepository extends BaseRepository<Pedido, Long> {
     List<Object[]> ingresosMensuales(@Param("mes") int mes);
 
     @Query(nativeQuery = true, value = "SELECT fecha_pedido AS Fecha, SUM(total) AS Ingresos, SUM(total_costo) AS " +
-            "Costos, SUM(total - total_costo) AS 'Ganancia Total' FROM Pedido p WHERE fecha_pedido BETWEEN " +
+            "Costos, SUM(total - total_costo) AS \"Ganancia Total\" FROM Pedido p WHERE fecha_pedido BETWEEN " +
             ":fechaDesde AND :fechaHasta GROUP BY fecha_pedido ORDER BY fecha_pedido")
     List<Object[]> ganancias(@Param("fechaDesde") LocalDate fechaDesde, @Param("fechaHasta") LocalDate fechaHasta);
 
-    @Query(nativeQuery = true, value = "SELECT c.id AS 'Nro Cliente', CONCAT(c.nombre, ' ', c.apellido) AS Cliente, " +
-            "COUNT(p.cliente_id) AS 'Cantidad Pedidos', s.nombre AS Sucursal FROM cliente c JOIN pedido p ON p" +
+    @Query(nativeQuery = true, value = "SELECT c.id AS \"Nro Cliente\", CONCAT(c.nombre, ' ', c.apellido) AS Cliente, " +
+            "COUNT(p.cliente_id) AS \"Cantidad Pedidos\", s.nombre AS Sucursal FROM cliente c JOIN pedido p ON p" +
             ".cliente_id = c.id JOIN sucursal s ON p.sucursal_id = s.id  WHERE p.fecha_pedido BETWEEN :fechaDesde AND" +
-            " :fechaHasta GROUP BY c.id ORDER BY COUNT(p.cliente_id) DESC;")
+            " :fechaHasta GROUP BY c.id, c.nombre, c.apellido, s.nombre ORDER BY COUNT(p.cliente_id) DESC")
     List<Object[]> pedidosPorCliente(@Param("fechaDesde") LocalDate fechaDesde,
                                      @Param("fechaHasta") LocalDate fechaHasta);
 }
